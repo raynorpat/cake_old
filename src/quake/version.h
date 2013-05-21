8b111fc766c4ca1a8881aa2c853cae3d571f92a3
Pat Raynor <raynorpat@gmail.com>
2013-05-21 19:13:27 -0400

    Upped version to 0.2

--- src/quake/version.h
+++ src/quake/version.h
@@ -22,7 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define _VERSION_H_
 
 #define	QW_VERSION	2.40
-#define PROGRAM_VERSION	"0.16"
+#define PROGRAM_VERSION	"0.2"
 
 #define RELEASE_VERSION
 
